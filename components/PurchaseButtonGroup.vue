f201341bb6d2eb9a9087df1dbf7d9546b92d3d51
Thiago Meira <thiago5meira@outlook.com>
2024-11-07 21:03:20 -0300

    feat: :sparkles: Ajustando tamanho dos cards com row-span e col-span no grid

--- components/PurchaseButtonGroup.vue
+++ components/PurchaseButtonGroup.vue
@@ -1,11 +1,14 @@
 <template>
     <div class="grid grid-cols-3 gap-4">
-        <Button v-for="value in purchaseOptions" :key="value">{{ value }}</Button>
+        <Button class="py-8 flex flex-col" v-for="value in purchaseOptions" :key="value">
+            <span class="text-2xl font-bold">{{ value }}</span>
+            <span class="font-light">SELECIONAR</span>
+        </Button>
     </div>
 </template>
 
 <script setup lang="ts">
 import { Button } from '@/components/ui/button'
 
-const purchaseOptions = ['+10', '+20', '+50', '+100', '+200', '+300']
+const purchaseOptions = ['+10', '+100', '+500', '+1000', '+2000', '+10000']
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -4,9 +4,9 @@
   </div>
 
   <section class="flex-1 container p-5">
-    <div class="grid gap-4 md:grid-cols-2 lg:grid-cols-8">
+    <div class="grid gap-4 md:grid-cols-2 lg:grid-cols-8 lg:grid-rows-3">
 
-      <Card class="col-span-3">
+      <Card class="lg:row-span-2 md:col-span-5 lg:col-span-3">
         <CardHeader class="flex flex-row items-center justify-between space-y-0 pb-2">
           <NuxtImg src="/img/exibe-numeros.png" class="w-full" />
           <!-- <Skeleton className="w-full bg-slate-50 rounded-xl" /> -->
@@ -23,10 +23,36 @@
       </Card>
 
       <UserStatsCard class="col-span-5" title="Cotas" description="Compre as cotas" icon="LucideTicket">
-        <CotasList />
-        <Separator orientation="horizontal" class="mt-5 mb-2" />
+        <!-- <CotasList /> -->
         <PurchaseButtonGroup />
+        <Separator orientation="horizontal" class="mt-5 mb-2" />
       </UserStatsCard>
+
+      <Card class="col-span-3 p-6 h-20">
+        <div class="flex justify-center items-center gap-4">
+          <Button variant="outline" size="icon" class="h-8 w-8 shrink-0 rounded-full" :disabled="goal <= 200"
+            @click="goal -= 1">
+            <MinusIcon class="h-4 w-4" />
+            <span class="sr-only">Decrease</span>
+          </Button>
+          <div class="text-2xl bg-secondary p-2 px-6 rounded-lg font-bold tracking-tighter">
+            {{ goal }}
+          </div>
+          <Button variant="outline" size="icon" class="h-8 w-8 shrink-0 rounded-full" :disabled="goal >= 400"
+            @click="goal += 1">
+            <PlusIcon class="h-4 w-4" />
+            <span class="sr-only">Increase</span>
+          </Button>
+        </div>
+      </Card>
+
+      <Button class="col-span-2 p-6 h-20 flex gap-4 justify-start">
+        <LucideShoppingCart />
+        <div class="flex flex-col">
+          <span class="font-bold">Participar</span>
+          <span class="text-sm">R$ 2.497,50</span>
+        </div>
+      </Button>
 
       <UserStatsCard class="col-span-4" title="Mais Compradores" description="Pessoas que compraram mais cotas na rifa"
         icon="LucidePartyPopper">
@@ -48,4 +74,23 @@ import TopBuyersList from '~/components/TopBuyersList.vue';
 import CardDescription from '~/components/ui/card/CardDescription.vue';
 import Separator from '~/components/ui/separator/Separator.vue'
 import UserStatsCard from '~/components/UserStatsCard.vue';
+
+const goal = ref(350)
+
+type Data = typeof data[number]
+const data = [
+  { goal: 400 },
+  { goal: 300 },
+  { goal: 200 },
+  { goal: 300 },
+  { goal: 200 },
+  { goal: 278 },
+  { goal: 189 },
+  { goal: 239 },
+  { goal: 300 },
+  { goal: 200 },
+  { goal: 278 },
+  { goal: 189 },
+  { goal: 349 },
+]
 </script>
